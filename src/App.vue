1d2a3b9a42fa57528e0defe73fd16ae2dbca8d7f
wanglin2 <1013335014@qq.com>
2023-04-08 15:25:56 +0800

    提交

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { watchEffect } from 'vue'
+import { Repl, ReplStore } from '@vue/repl'
+import '@vue/repl/style.css'
+
+const query = new URLSearchParams(location.search)
+const store = new ReplStore({
+  serializedState: location.hash.slice(1),
+
+  showOutput: query.has('showOutput'),
+  outputMode: (query.get('outputMode') || 'preview'),
+})
+
+watchEffect(() => history.replaceState({}, '', store.serialize()))
+
+store.setImportMap({
+  imports: {
+    "vue": "https://play.vuejs.org/vue.runtime.esm-browser.js",
+    "simple-mind-map": "./simpleMindMap.esm.js"
+  }
+})
+</script>
+
+<template>
+  <div class="replContainer">
+    <Repl style="height: 100%" :store="store" :ssr="false" />
+  </div>
+</template>
+
+<style scoped>
+.replContainer {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+}
+</style>
